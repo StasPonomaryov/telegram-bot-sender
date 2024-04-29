6ef2a8675010cb13a78e4d585ec9f277764541d9
StasPonomaryov <ponomaryov.stas@gmail.com>
2024-04-29 17:22:34 +0300

    feat: add simpleMarkdown utility to preview Telegram markdown

--- lib/utils.ts
+++ lib/utils.ts
@@ -29,3 +29,20 @@ USER: ${chat_id}
 export function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+export function simpleMarkdown(mdText: string) {
+  const formatMD = (mdstr: string) => {
+    mdstr = mdstr.replace(/\[(.*?)\]\((.*?)\)/gm, '<a href="$2">$1</a>');
+    mdstr = mdstr.replace(/\*(\w.*?[^\\])\*/gm, '<b>$1</b>');
+    mdstr = mdstr.replace(/_(\w.*?[^\\])_/gm, '<em>$1</em>');
+    mdstr = mdstr.replace(/__(\w.*?[^\\])__/gm, '<u>$1</u>');
+    mdstr = mdstr.replace(/~(\w.*?)~/gm, '<s>$1</s>');
+    mdstr = mdstr.replace(/\|\|(\w.*?[^\\])\|\|/gm, '<span>&#9617;</span>');
+    mdstr = mdstr.replace(/  \n/g, '\n<br/>').replace(/\n\s*\n/g, '\n<p>\n');
+
+    return mdstr.replace(/\\([`_~\*\+\-\.\^\\\<\>\(\)\[\]])/gm, '$1');
+  };
+  const markdownText = mdText.replace(/\r\n/g, '\n').replace(/\n~~~/g, '\n```');
+
+  return formatMD(markdownText);
+}
