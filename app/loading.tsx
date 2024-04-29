ee04d48337eb64c44863725d1ce3a9f6175199c8
StasPonomaryov <ponomaryov.stas@gmail.com>
2024-04-29 17:23:06 +0300

    chore: proper path in loading page

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next';
-import LoadingSpinner from './components/LoadingSpinner';
+import LoadingSpinner from './components/UI/LoadingSpinner';
 
 const Loading: NextPage = () => {
   return (
